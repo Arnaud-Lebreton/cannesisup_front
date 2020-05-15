801d5938f9cce43831542b3f8cccc6fd725993e6
Vadim Ziboni <vadimziboni@gmail.com>
2020-05-15 17:08:08 +0200

    Fri 05/15/2020

--- src/components/profil/Profil.js
+++ src/components/profil/Profil.js
@@ -14,6 +14,7 @@ class Profil extends Component{
             email : "",
             work : "",
             work_description : "",
+            isShow : true,
         }
 
         this.handleChange = this.handleChange.bind(this);
