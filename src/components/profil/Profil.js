8705ff6ffdac897030dd4e8f150000e84bdd9725
Vadim Ziboni <vadimziboni@gmail.com>
2020-05-17 18:20:22 +0200

    Sun 05/17/2020

--- src/components/profil/Profil.js
+++ src/components/profil/Profil.js
@@ -29,55 +29,60 @@ class Profil extends Component{
     render(){
         return (
             <div>
-                <div id = "middle_bloc">
-                    <div>
-                        <img id= "image_Fond"/>
-                        <input type="file" />
+                <div id="body">
+                    <div id = "middle_bloc">
+                        <div id="image_fond">
+                            <img/>
+                            <button>Ajouter Image</button>
+                        </div>
+            
+                        <div id= "image_Logo" >
+                            <img src={require("../Images/Untitled.png")}/>
+                            <button>Ajouter Image</button>
+                        </div>
+                        <div>
+                            <h2>{this.state.society_name}</h2>
+                            <input placeholder = "Nom de Votre Societé..." disabled={this.state.isDisabled}/>
+                            <textarea placeholder = "Parlez-nous de celle-ci..."  disabled={this.state.isDisabled}/>
+                            <input placeholder = "Son Secteur d'Activité..."  disabled={this.state.isDisabled}/>
+                        </div>
+                        <div>
+                            <h2>Coordonnées : </h2>
+                            <input type = "email" placeholder = "Email" disabled={this.state.isDisabled} />
+                            <input type = "text" placeholder = "Adresse" disabled={this.state.isDisabled} />
+                            <input type = "tel" placeholder = "Telephone" disabled={this.state.isDisabled}/>
+                            <a href = {this.state.society_website}></a>
+                            <input type = "text" placeholder = "Site" disabled={this.state.isDisabled} />
+                        </div>
+                        <div>
+                            <h2>Reseaux Sociaux : </h2>
+                            <a>{this.state.society_website}</a>
+                            <button>Ajouter Reseaux</button>
+                        </div>
                     </div>
-        
-                    <div>
-                        <img id= "image_Logo" />
-                        <input type="file" />
-                    </div>
-                    <div>
-                        <h2>{this.state.society_name}</h2>
-                        <input placeholder = "Nom de Votre Societé..." value= "BABA" disabled={this.state.isDisabled}/>
-                        <textarea placeholder = "Parlez-nous de celle-ci..." value= {this.state.society_description} disabled={this.state.isDisabled}/>
-                        <input placeholder = "Son Secteur d'Activité..." value= {this.state.society_activity_sector} disabled={this.state.isDisabled}/>
-                    </div>
-                    <div>
-                        <h2>Coordonnées : </h2>
-                        <input type = "email" placeholder = "Email" value= {this.state.email} disabled={this.state.isDisabled} />
-                        <input type = "text" placeholder = "Adresse" value= {this.state.society_adress} disabled={this.state.isDisabled} />
-                        <input type = "tel" placeholder = "Telephone" value= {this.state.phone_number} disabled={this.state.isDisabled}/>
-                        <a href = {this.state.society_website}></a>
-                        <input type = "text" placeholder = "Site" value= {this.state.society_website} disabled={this.state.isDisabled} />
-                    </div>
-                    <div>
-                        <h2>Reseaux Sociaux : </h2>
-                        <a>{this.state.society_website}</a>
+                    <div id = "side_bloc">
+                        <div>
+                            <img id = "image_Profil"/>
+                            <a>Ajouter une Photo de Profil</a>
+                        </div>
+                        <div>
+                            <h2>{this.state.name}</h2>
+                            <input placeholder="Nom et Prenom" disabled={this.state.isDisabled}/>
+                        </div>
+                        <div>
+                            <h2>{this.state.work} </h2>
+                            <input placeholder="Profession" disabled={this.state.isDisabled}/>
+                        </div>
+                        <div>
+                            <h2>{this.state.work_description}</h2>
+                            <textarea placeholder = "Description..." disabled={this.state.isDisabled} />
+                        </div>
+                        <button onClick = {() => this.hide(false)}>Modifier</button>
+                        <button onClick = {() => this.hide(true)}>Confirmer</button>
                     </div>
                 </div>
-                <div id = "side_bloc">
-                    <div>
-                        <img id = "image_Profil"/>
-                        <a>Ajouter une Photo de Profil</a>
-                    </div>
-                    <div>
-                        <h2>{this.state.name}</h2>
-                        <input placeholder="Nom et Prenom" value={this.state.name} disabled={this.state.isDisabledfalse}/>
-                    </div>
-                    <div>
-                        <h2>{this.state.work} </h2>
-                        <input placeholder="Profession" value= {this.state.work} disabled={this.state.isDisabled}/>
-                    </div>
-                    <div>
-                        <h2>{this.state.work_description}</h2>
-                        <textarea placeholder = "Description..." value= {this.state.work_description} disabled={this.state.isDisabled} />
-                    </div>
-                    <button onClick = {() => this.hide(false)}>Modifier</button>
-                </div>
-                <button onClick = {() => this.hide(true)}>Confirmer</button>
+                
+                
             </div>
         )
     }
