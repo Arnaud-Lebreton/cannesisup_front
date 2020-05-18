c43f61dd6266df264c0ff7fad78b0f3f74395b3e
Vadim Ziboni <vadimziboni@gmail.com>
2020-05-18 14:56:19 +0200

    Mon 05/18/2020

--- src/components/profil/Profil.js
+++ src/components/profil/Profil.js
@@ -17,8 +17,10 @@ class Profil extends Component{
             work : "",
             work_description : "",
             isShow : true,
-            isDisabled:false,
-            isHidden: "",
+            isDisabled:true,
+            image_profil:"",
+            image_fond: "",
+            image_logo:"",
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -27,7 +29,10 @@ class Profil extends Component{
         this.setState({isDisabled : a});
     }
     handleChange(event){
-        this.setState({value : event.target.value});
+        const value = event.target.value;
+        setState({
+            
+        });
     }
     render(){
         return (
@@ -35,50 +40,56 @@ class Profil extends Component{
                 <div id="body">
                     <div id = "middle_bloc">
                         <div id="image_fond">
-                            <img />
+                            <img src={this.state.image_fond}/>
                             <button>Ajouter Image</button>
                         </div>
                         <div id= "image_Logo" >
-                            <img src={require("../Images/Untitled.png")}/>
+                            <img src={require("../Images/Untitled.png")} {/*Mettre en source la data*/}/>
                             <button>Ajouter Image</button>
                         </div>
                         <div>
-                            <input placeholder = "Nom de Votre Societé..." disabled={this.state.isDisabled}/>
-                            <textarea placeholder = "Parlez-nous de celle-ci..."  disabled={this.state.isDisabled}/>
-                            <input placeholder = "Son Secteur d'Activité..."  disabled={this.state.isDisabled}/>
+                            <input placeholder = "Nom de Votre Societé..." disabled={this.state.isDisabled} name="society_name"/>
+                            <textarea placeholder = "Parlez-nous de celle-ci..."  disabled={this.state.isDisabled} name="society_info"/>
+                            <input placeholder = "Son Secteur d'Activité..."  disabled={this.state.isDisabled} name="society_sector"/>
                         </div>
                         <div>
-                            <h2>Coordonnées : </h2>
+                            <h3>Coordonnées : </h3>
+                            <label>Email : </label>
                             <input type = "email" placeholder = "Email" disabled={this.state.isDisabled} />
+                            <label>Adresse : </label>
                             <input type = "text" placeholder = "Adresse" disabled={this.state.isDisabled} />
+                            <label>Telephone : </label>
                             <input type = "tel" placeholder = "Telephone" disabled={this.state.isDisabled}/>
+                            <label>Site : </label>
                             <a href = {this.state.society_website}></a>
                             <input type = "text" placeholder = "Site" disabled={this.state.isDisabled} />
                         </div>
                         <div>
-                            <h2>Reseaux Sociaux : </h2>
+                            <h3>Reseaux Sociaux : </h3>
                             <a>{this.state.society_website}</a>
                             <button>Ajouter Reseaux</button>
                         </div>
                     </div>
                     <div id = "side_bloc">
                         <div id="profil_image">
-                            <img src={require("../Images/img_avatar.png")}/>
+                            <img src={require("../Images/img_avatar.png")} {/*Mettre en source la data*/}/>
                             <button>Ajouter Photo</button>
                         </div>
                         <div>
-                            <input placeholder="Nom et Prenom" disabled={this.state.isDisabled}/>
+                            <input placeholder="Nom et Prenom..." disabled={this.state.isDisabled} name="name" value={this.state.name} onChange={this.handleChange}/>
                         </div>
                         <div>
-                            <input placeholder="Profession" disabled={this.state.isDisabled}/>
+                            <input placeholder="Profession..." disabled={this.state.isDisabled} name="profession"/>
                         </div>
                         <div>
-                            <textarea placeholder = "Description..." disabled={this.state.isDisabled} />
+                            <h3>Parole de membre</h3>
+                            <textarea placeholder = "Description..." disabled={this.state.isDisabled} name="description" />
                         </div>
                         <div id="interaction">
-                            <button onClick = {() => this.hide(false)}>Modifier</button>
-                            <button onClick = {() => this.hide(true)}>Confirmer</button>
+                            {this.state.isDisabled && <button onClick = {() => this.hide(false)} >Modifier</button>}
+                            {!this.state.isDisabled && <button type="submit" onClick = {() => this.hide(true)}>Confirmer</button>}
                         </div>
+                        <p>{this.state.name}</p>
                     </div>
                 </div>  
             </div>
